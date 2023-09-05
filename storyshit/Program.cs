fd8b68b7d5e703d82d879caab7b9eaf478289ea9
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-09-05 10:25:29 +0200

    if/else sattser

---
+++ storyshit/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Hello, World!");
+string name = Console.ReadLine().ToLower();
+
+if (name == "boyo")
+{
+    Console.WriteLine("välkommen");
+}
+else if (name == "julius")
+{
+    Console.Write("hello");
+}
+else
+{
+    Console.WriteLine("ur gay");
+}
+
+
+
+
+Console.ReadLine();
